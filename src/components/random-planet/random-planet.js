de4bc195a2e4ea13441647dc8f77264a89c7e50a
zybzick <arclanov94@gmail.com>
2020-07-13 19:09:51 +0500

    Item list

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -16,9 +16,13 @@ export default class RandomPlanet extends Component {
     error: false
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet()
+    this.interval = setInterval(this.updatePlanet, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   onPlanetLoaded = (planet) => {
@@ -35,8 +39,8 @@ export default class RandomPlanet extends Component {
     })
   }
 
-  updatePlanet(){
-    const id = Math.floor(Math.random()*25) + 2;
+  updatePlanet = ()=>{
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService
         .getPlanet(id)
         .then(this.onPlanetLoaded)
@@ -59,7 +63,10 @@ export default class RandomPlanet extends Component {
         </div>
     );
   }
+
 }
+
+
 
 const PlanetView = ({ planet }) => {
 
